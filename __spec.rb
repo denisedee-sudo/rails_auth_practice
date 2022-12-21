7f35dff775948a5fdb33841979554710ab5793b4
codecademy <ccuser@codecademy.com>
2022-12-21 21:22:28 +0000

    creating logout session

--- __spec.rb
+++ __spec.rb
@@ -4,10 +4,17 @@ require 'rails_helper'
 describe SessionsController, :type => :controller do
   let(:controller) { SessionsController.new }
 
-  context "user create action" do
-    it "should have a create action" do
-      expect(controller).to respond_to(:create), "Did you implement the create action?"
+  context "session destroy action" do
+    it "should have a destroy action" do
+      expect(controller).to respond_to(:destroy), "Did you implement the destroy action?"
     end
 
+    it "should remove user id from session" do
+      session[:user_id] = 1
+
+      response =  post :destroy
+      expect(session[:user_id]).to be(nil)
+      expect(response).to redirect_to('/')
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to '/'
+        session[:user_id] = @user.id
+        redirect_to '/'
     else
-      redirect_to 'login'
+        redirect_to 'login'
     end 
   end 
+  def destroy 
+    session[:user_id] = nil 
+    redirect_to '/' 
+  end
 end
